e37467da20f2903b33cfa835430df153f91ed8b8
TTCode10 <ttcode10@gmail.com>
2021-02-03 23:52:08 +0800

    🚀 build

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const cors = require('cors');
 const express = require('express');
+const path = require('path');
 const connectDB = require('./utils/db');
 const routes = require('./routes');
 
@@ -10,7 +11,11 @@ connectDB();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'build')));
 app.use('/api', routes);
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 const PORT = process.env.PORT || 5000;
 
